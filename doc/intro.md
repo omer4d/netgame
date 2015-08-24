ccc8bc36560645e78ed0766f45afab50876e3399
Stas <omer4d@gmail.com>
2015-08-24 08:13:00 +0300

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to netgame
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/netgame/core_test.clj
@@ -0,0 +1,7 @@
+(ns netgame.core-test
+  (:require [clojure.test :refer :all]
+            [netgame.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
