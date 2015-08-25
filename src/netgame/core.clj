0a8b38d5e878454a472f64616577467c7fdd6690
Stas <omer4d@gmail.com>
2015-08-25 13:53:16 +0300

    client-server split

--- src/netgame/core.clj
+++ src/netgame/core.clj
@@ -55,14 +55,14 @@
             field-names
             field-types
             (range))))))
-  
-(defn gen-apply-diff [name field-names field-types]
+
+(defn gen-apply-diff [name client-rec-name field-names field-types]
   (let [stream-sym (tagged-sym 'stream `DataInputStream)
-        old-sym (tagged-sym 'old name)]
+        old-sym (tagged-sym 'old client-rec-name)]
     `(defmethod apply-diff '~name [~'type ~stream-sym ~old-sym]
        (let [~'flags (. ~stream-sym ~'readByte)]
          (new
-          ~name
+          ~client-rec-name
           ~@(map
              (fn [field type index]
                `(if (bit-test ~'flags ~index)
@@ -72,40 +72,81 @@
              field-types
              (range)))))))
 
-(defmacro def-net-struct [name & fields]
-  `(do
-     (defrecord ~name ~(apply vector (map #(tagged-sym (first %) (second %)) fields)))
-     ~(gen-write-diff name (map first fields) (map second fields))
-     ~(gen-apply-diff name (map first fields) (map second fields))))
+(defn plist->map [plist]
+  (into {} (map #(into [] %)(partition 2 plist))))
+
+(defn canonize-list-field [field]
+  {:name (first field)
+   :type (second field)
+   :options (plist->map (drop 2 field))})
+
+(defn canonize-nf-field [field]
+  (canonize-list-field field))
+
+(defn canonize-sf-field [field]
+  (if (list? field)
+    (canonize-list-field field)
+    {:name field
+     :type 'Object
+     :options {}}))
+
+(defmacro def-net-struct [struct-name & field-group-plist]
+  (let [field-groups (plist->map field-group-plist)
+        canon-nf-fields (map canonize-nf-field (:net field-groups))
+        canon-sf-fields (map canonize-sf-field (:server field-groups))
+        client-rec-name (symbol (str (name struct-name) "Client"))]
+    `(do
+       (defrecord ~struct-name ~(apply vector (map #(tagged-sym (:name %) (:type %)) (concat canon-nf-fields canon-sf-fields))))
+       (defrecord ~client-rec-name ~(apply vector (map #(tagged-sym (:name %) (:type %)) canon-nf-fields)))
+       ~(gen-write-diff struct-name (map :name canon-nf-fields) (map :type canon-nf-fields))
+       ~(gen-apply-diff struct-name client-rec-name (map :name canon-nf-fields) (map :type canon-nf-fields)))))
+
+;;
+;; Tests
+;;
+
+'(def-net-struct Foo
+   :net [(a int)
+         (b short)
+         (c short)
+         (d int :interp linear)]
+   :server [(x)
+            (y)
+            (z int)])
 
 (macroexpand-1 '(def-net-struct Baz
-                  (a int)
-                  (b short)
-                  (c short)
-                  (d int)))
+                  :net [(a int)
+                        (b short)
+                        (c short)
+                        (d int)]
+                  :server [x y z]))
 
 (macroexpand-1 '(def-net-struct Bar
-                  (x short)
-                  (y short)
-                  (baz Baz)))
+                  :net [(x short)
+                        (y short)
+                        (baz Baz)]))
 
 (def-net-struct Baz
-  (a int)
-  (b short)
-  (c short)
-  (d int))
+  :net
+  [(a int)
+   (b short)
+   (c short)
+   (d int)]
+  :server
+  [x y z])
 
 (def-net-struct Bar
-  (x short)
-  (y short)
-  (baz Baz))
+  :net
+  [(x short)
+   (y short)
+   (baz Baz)])
 
 (let [bos (new ByteArrayOutputStream)
       dos (new DataOutputStream bos)
-      baz (Baz. 1 2 3 4)]
+      baz (Baz. 1 2 3 4 'a 'b 'c)]
   (write-diff 'Bar dos (Bar. 1 1 baz) (Bar. 2 2 (assoc baz :a 10) ))
   (let [arr (. bos toByteArray)
         bis (new ByteArrayInputStream arr)
         dis (new DataInputStream bis)]
     (print (count arr))
-    (apply-diff 'Bar dis (Bar. 1 1 baz))))
+    (apply-diff 'Bar dis (BarClient. 1 1 (BazClient. 1 2 3 4)))))
