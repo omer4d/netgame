0059a50830b42e80718344647aff33665a87d215
Stas <omer4d@gmail.com>
2015-08-26 08:39:00 +0300

    full updates + fixed project config

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :repl-options {:init-ns netgame.core
+				 :init (do (set! *warn-on-reflection* true)
+						   (set! *unchecked-math* :warn-on-boxed))})
--- src/netgame/core.clj
+++ src/netgame/core.clj
@@ -1,8 +1,19 @@
 (ns netgame.core
-  (:import [java.io DataInputStream DataOutputStream ByteArrayInputStream ByteArrayOutputStream]))
+  (:import [java.io DataInputStream DataOutputStream ByteArrayInputStream ByteArrayOutputStream])
+  (:use clojure.data))
 
+(defmulti write-bin (fn [& args] (first args)))
+(defmulti read-bin (fn [& args] (first args)))
 (defmulti write-diff (fn [& args] (first args)))
 (defmulti apply-diff (fn [& args] (first args)))
+
+;; Int
+
+(defmethod write-bin 'int [type ^DataOutputStream stream x]
+  (. stream writeInt x))
+
+(defmethod read-bin 'int [type ^DataInputStream stream]
+  (. stream readInt))
 
 (defmethod write-diff 'int [type ^DataOutputStream stream from to]
   (. stream writeInt to))
@@ -10,11 +21,27 @@
 (defmethod apply-diff 'int [type ^DataInputStream stream old]
   (. stream readInt))
 
+;; Short
+
+(defmethod write-bin 'short [type ^DataOutputStream stream x]
+  (. stream writeShort x))
+
+(defmethod read-bin 'short [type ^DataInputStream stream]
+  (. stream readShort))
+
 (defmethod write-diff 'short [type ^DataOutputStream stream from to]
   (. stream writeShort to))
 
 (defmethod apply-diff 'short [type ^DataInputStream stream old]
   (. stream readShort))
+
+;; Byte
+
+(defmethod write-bin 'byte [type ^DataOutputStream stream x]
+  (. stream writeByte x))
+
+(defmethod read-bin 'byte [type ^DataInputStream stream]
+  (. stream readByte))
 
 (defmethod write-diff 'byte [type ^DataOutputStream stream from to]
   (. stream writeByte to))
@@ -33,6 +60,27 @@
 
 (defn tagged-sym [sym tag]
   (with-meta sym {:tag tag}))
+
+(defn gen-write-bin [name field-names field-types]
+  (let [stream-sym (tagged-sym 'stream `DataOutputStream)
+        x-sym (tagged-sym 'x name)]
+    `(defmethod write-bin '~name [~'type ~stream-sym ~x-sym]
+       ~@(map
+          (fn [field type]
+            `(write-bin '~type ~stream-sym (. ~x-sym ~field)))
+          field-names
+          field-types))))
+
+(defn gen-read-bin [name client-rec-name field-names field-types]
+  (let [stream-sym (tagged-sym 'stream `DataInputStream)]
+    `(defmethod read-bin '~name [~'type ~stream-sym]
+       (new
+        ~client-rec-name
+        ~@(map
+           (fn [field type]
+             `(read-bin '~type ~stream-sym))
+           field-names
+           field-types)))))
 
 (defn gen-write-diff [name field-names field-types]
   (let [stream-sym (tagged-sym 'stream `DataOutputStream)
@@ -99,7 +147,9 @@
        (defrecord ~struct-name ~(apply vector (map #(tagged-sym (:name %) (:type %)) (concat canon-nf-fields canon-sf-fields))))
        (defrecord ~client-rec-name ~(apply vector (map #(tagged-sym (:name %) (:type %)) canon-nf-fields)))
        ~(gen-write-diff struct-name (map :name canon-nf-fields) (map :type canon-nf-fields))
-       ~(gen-apply-diff struct-name client-rec-name (map :name canon-nf-fields) (map :type canon-nf-fields)))))
+       ~(gen-apply-diff struct-name client-rec-name (map :name canon-nf-fields) (map :type canon-nf-fields))
+       ~(gen-write-bin struct-name (map :name canon-nf-fields) (map :type canon-nf-fields))
+       ~(gen-read-bin struct-name client-rec-name (map :name canon-nf-fields) (map :type canon-nf-fields)))))
 
 ;;
 ;; Tests
@@ -141,6 +191,15 @@
    (y short)
    (baz Baz)])
 
+;; TODO:
+;; full updates - done
+;; interpolation
+;; int map diffs
+;; def-net-msg
+;; syntax validation + option validation + ensure net struct has at least 1 net property
+;; flag 0 -> field changed to nil
+;; add options param
+
 (let [bos (new ByteArrayOutputStream)
       dos (new DataOutputStream bos)
       baz (Baz. 1 2 3 4 'a 'b 'c)]
@@ -150,3 +209,12 @@
         dis (new DataInputStream bis)]
     (print (count arr))
     (apply-diff 'Bar dis (BarClient. 1 1 (BazClient. 1 2 3 4)))))
+
+(let [bos (new ByteArrayOutputStream)
+      dos (new DataOutputStream bos)
+      baz (Baz. 1 2 3 4 'a 'b 'c)]
+  (write-bin 'Bar dos (Bar. 1 1 baz))
+  (let [arr (. bos toByteArray)
+        bis (new ByteArrayInputStream arr)
+        dis (new DataInputStream bis)]
+    (read-bin 'Bar dis)))
