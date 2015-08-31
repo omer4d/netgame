8ba36446dcac46bc7d00a8f09b8d8d51f25c289a
Stas <omer4d@gmail.com>
2015-08-31 13:20:58 +0300

    cl+sv local

--- src/netgame/game.clj
+++ src/netgame/game.clj
@@ -21,6 +21,31 @@
                       `(. ~tmp-sym ~%))
                    basis)))))
 
+(defn fmap [f m]
+  (into (empty m) (for [[k v] m] [k (f v)])))
+
+(defn randf ^double [^double min ^double max]
+  (+ min (* (double (rand)) (- max min))))
+
+(defn rand-sig ^double []
+  (if (< (double (rand)) 0.5) 1.0 -1.0))
+
+(defn in-range? [x min max]
+  (and (>= x min) (<= x max)))
+
+(defn clamp [x min max]
+  (if (< x min)
+    min
+    (if (> x max)
+      max
+      x)))
+
+(defn nano->sec ^double [^long x] (/ (double x) 1000000000.0))
+
+;; *************************
+;; * Game State Structures *
+;; *************************
+
 (def-net-struct Entity
   :net
   [(x float)
@@ -37,12 +62,6 @@
   :net
   [(ents (short-map :elem-type Entity))])
 
-(defn randf ^double [^double min ^double max]
-  (+ min (* (double (rand)) (- max min))))
-
-(defn rand-sig ^double []
-  (if (< (double (rand)) 0.5) 1.0 -1.0))
-
 (defn create-entity [x y]
   (Entity. x y
            (randf 10 30)
@@ -55,31 +74,76 @@
 (defn create-game [n]
   (Game. (zipmap (range)
                  (take n (repeatedly #(create-entity (randf 0 640) (randf 0 480)))))))
-  
-(defn update-entity [^Entity ent ^double dt]
-  (with-fields ent [x y vx vy]
-    (updated-instance Entity ent [x y vy]
-                     (+ x (* vx dt))
-                     (+ y (* vy dt))
-                     (+ vy (* 10 dt)))))
+
+(defn move [{:keys [x y vx vy] :as ent} dt]
+  (assoc ent
+         :x (+ x (* vx dt))
+         :y (+ y (* vy dt))))
+
+(defn accel [{:keys [vx vy] :as ent} ax ay dt]
+  (assoc ent
+         :vx (+ vx (* ax dt))
+         :vy (+ vy (* ay dt))))
+
+(defn bounce [{:keys [x y vx vy] :as ent} minx miny maxx maxy dt]
+  (assoc ent
+         :x (clamp x minx maxx)
+         :y (clamp y miny maxy)
+         :vx (if (in-range? x minx maxx) vx (* vx -1))
+         :vy (if (in-range? y miny maxy) vy (* vy -1))))
+
+(defn update-entity [{rad :rad :as ent} dt]
+  (-> ent (accel 0 100 dt) (move dt) (bounce rad rad (- 640 rad) (- 480 rad) dt)))
+
+(defn update-game [game dt]
+  (assoc game :ents (fmap #(update-entity % dt) (:ents game))))
+
+(defn draw-ent [{:keys [x y rad] :as ent}]
+  (q/ellipse x y (* rad 2) (* rad 2)))
+
+(defn draw-game [game]
+  (doseq [[_ ent] (:ents game)]
+    (draw-ent ent)))
+
+;; *************
+;; * Processes *
+;; *************
+
+(defrecord ServerState [clients game])
+
+(def game-state (atom (create-game 10)))
+
+(def server (atom nil))
+
+(defn stop-server! []
+  (future-cancel @server))
+
+(defn start-server! [target-dt]
+  (when-not (or (nil? @server) (future-done? @server))
+    (stop-server!))
+  (reset! server
+          (future (loop [last-tick-time 0]
+                    (let [curr-time (System/nanoTime)]
+                      (when-not (Thread/interrupted)
+                        (if (>= (nano->sec (- curr-time last-tick-time)) target-dt)
+                          (do
+                            (swap! game-state update-game target-dt)
+                            (recur curr-time))
+                          (recur last-tick-time))))))))
 
 (defn setup []
   (q/smooth)
-  (q/frame-rate 60)
-  (q/background 200))
+  (q/frame-rate 60))
 
 (defn draw []
-  (q/stroke (q/random 255))
-  (q/stroke-weight (q/random 10))
-  (q/fill (q/random 255))
+  (q/background 200)
+  (draw-game @game-state))
 
-  (let [diam (q/random 100)
-        x    (q/random (q/width))
-        y    (q/random (q/height))]
-    (q/ellipse x y diam diam)))
+(defn start-client! []
+  (q/defsketch example
+    :title "Oh so many grey circles"
+    :setup setup
+    :draw draw
+    :size [640 480]))
 
-(q/defsketch example
-  :title "Oh so many grey circles"
-  :setup setup
-  :draw draw
-  :size [323 200])
+;(start-server! 0.025)
